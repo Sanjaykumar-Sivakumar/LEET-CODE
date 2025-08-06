5d001e9ddd2c0f667d24397fe52193b68d06a2d8
SANJAYKUMAR S <sanjays23112004@gmail.com>
2025-08-06 18:21:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (42.41%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num == 0)
+        {
+            return 0;
+        }
+        int ans = num%9;
+        return (ans==0)? 9 : ans; 
+
+    }
+}
