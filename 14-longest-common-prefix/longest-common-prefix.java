b47f2b749713ea16c50394309eedc80dcbcddafa
SANJAYKUMAR S <sanjays23112004@gmail.com>
2025-08-10 19:37:18 +0530

    Time: 7 ms (12.61%) | Memory: 42.3 MB (9.78%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String res ="";
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        for(int i = 0;i<first.length();i++)
+        {
+            if(first.charAt(i) == last.charAt(i))
+            {
+                res+=first.charAt(i);
+            }
+            else
+            {
+                break;
+            }
+        }
+        return res;
+    }
+}
