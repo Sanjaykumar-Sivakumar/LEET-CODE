782ec3ed1ec8ecad0381fc50abfe84e79ddcf443
SANJAYKUMAR S <sanjays23112004@gmail.com>
2025-10-06 22:05:14 +0530

    Time: 9 ms (30.63%) | Memory: 44.7 MB (34.75%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        s = s.replaceAll("\\s","").toLowerCase();
+        t = t.replaceAll("\\s","").toLowerCase();
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        if(Arrays.equals(c1,c2) && c1.length == c2.length)
+        {
+            return true;
+        }
+        return false;
+    }
+}
