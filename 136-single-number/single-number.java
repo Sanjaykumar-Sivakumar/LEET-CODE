3cf3a67649bdd265f85c8cd1e04ce605c5e798aa
SANJAYKUMAR S <sanjays23112004@gmail.com>
2025-10-09 23:05:33 +0530

    Time: 1 ms (99.87%) | Memory: 46.8 MB (5.86%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,21 +1,29 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> s = new HashSet<>();
-        for(int n : nums)
+        //XOR Approach (Best solution — O(n) time, O(1) space)
+        int ans = 0;
+        for(int i : nums)
         {
-            if(s.contains(n))
-            {
-                s.remove(n);
-            }
-            else
-            {
-                s.add(n);
-            }
+            ans ^= i;
         }
-        for(int a : s)
-        {
-            return a;
-        }
-        return -1;
+        return ans;
+        //Set Based Approach (O(n) time, O(n) space)
+        // Set<Integer> s = new HashSet<>();
+        // for(int n : nums)
+        // {
+        //     if(s.contains(n))
+        //     {
+        //         s.remove(n);
+        //     }
+        //     else
+        //     {
+        //         s.add(n);
+        //     }
+        // }
+        // for(int a : s)
+        // {
+        //     return a;
+        // }
+        // return -1;
     }
 }
