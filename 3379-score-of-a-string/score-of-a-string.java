0e51b7f3a37d02823a5a1cb92f4b3bc5f4fcd621
SANJAYKUMAR S <sanjays23112004@gmail.com>
2025-10-26 09:09:11 +0530

    Time: 1 ms (99.36%) | Memory: 42.2 MB (46.72%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sc = 0;
+        for(int i = 1;i<s.length();i++)
+        {
+            sc += Math.abs(s.charAt(i) - s.charAt(i - 1));
+        }
+        return sc;
+    }
+}
