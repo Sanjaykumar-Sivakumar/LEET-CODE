5484243b7ba042bbe110155d1a9f74d87d802094
SANJAYKUMAR S <sanjays23112004@gmail.com>
2025-12-02 20:12:06 +0530

    Time: 15 ms (54.10%) | Memory: 93.3 MB (31.71%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,23 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0;i<nums.length - 1;i++)
+        HashSet <Integer> s = new HashSet<>();
+        for(int n : nums)
         {
-            if(nums[i] == nums[i + 1])
+            if(s.contains(n))
             {
                 return true;
             }
+            s.add(n);
         }
         return false;
+        // Arrays.sort(nums);
+        // for(int i = 0;i<nums.length - 1;i++)
+        // {
+        //     if(nums[i] == nums[i + 1])
+        //     {
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 }
